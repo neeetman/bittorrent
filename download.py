e9e124998a21517ab942e64769e7341c5aafb9f0
neeetman <anshaohac@gmail.com>
2020-11-09 20:56:42 +0800

    new

--- download.py
+++ download.py
@@ -3,6 +3,8 @@ import logging
 import struct
 import time
 
+from hashlib import sha1
+
 from torrent import TorrentInfo, DownloadInfo
 from tracker import Tracker
 from peer import Peer
@@ -16,25 +18,25 @@ async def download(torrent_file, download_path):
 
     files = [download_info.files[0], ] # download first file
 
-    received_blocks = asyncio.Queue()
+    received_pieces_queue = asyncio.Queue()
     download_info.select_files(files)
-    file_writer = FileSaver(torrent_info, files[0], received_blocks)
+    file_writer = FileSaver(torrent_info, files[0], received_pieces_queue)
 
     peers_info = await tracker.request_peers()
 
     sessions = list()
     for peer in peers_info:
-        session = DownloadSession(torrent_info, received_blocks, peer)
+        session = DownloadSession(torrent_info, received_pieces_queue, peer)
         sessions.append(session)
 
     await asyncio.gather(*[session.download() for session in sessions])
 
 
 class DownloadSession(object):
-    def __init__(self, torrent: TorrentInfo, received_blocks, peer: Peer):
+    def __init__(self, torrent: TorrentInfo, received_pieces_queue, peer: Peer):
         self.torrent = torrent
         self.peer = peer
-        self._received_blocks = received_blocks
+        self._queue = received_pieces_queue
 
         self._total_downloaded = 0
         self._last_download_time = time.time()
@@ -48,6 +50,25 @@ class DownloadSession(object):
             self.torrent.download_info.info_hash,
             self.torrent.my_peer_id.encode()
         )
+
+    def save_block_received(self, piece_idx, begin, data):
+        piece = self.torrent.download_info.pieces[piece_idx]
+        piece.save_block(begin, data)
+
+        if not piece.is_complete():
+            return
+
+        #get the whole piece data
+        piece_data = piece.data
+
+        #check the hash value
+        hash_recv = sha1(piece_data).digest()
+        if hash_recv != piece.piece_hash:
+            logging.error("Hash of piece %s is wrong" % piece_idx)
+            piece.flush()
+            return
+
+        self._queue.put_nowait(piece_idx * self.torrent.download_info.piece_length, piece_data)
 
     async def download(self):
         retries = 0
